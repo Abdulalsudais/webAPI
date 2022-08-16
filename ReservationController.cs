1b6a82fe4b193f58083c71be61dcd354ee23426c
Abdulalsudais <91174429+Abdulalsudais@users.noreply.github.com>
2022-08-16 11:39:11 +0530

    Add files via upload

---
+++ ReservationController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APIController.Models;
+using Microsoft.AspNetCore.JsonPatch;
+
+namespace APIController.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+        private iRepository repository;
+        public ReservationController(iRepository repo)
+        {
+            repository = repo;
+        }
+
+        [HttpGet]
+        public IEnumerable<Reservation> Get() => repository.Reservations;
+
+        [HttpGet("{id}")]
+        public ActionResult<Reservation> Get(int id)
+        {
+            if (id == 0)
+                return BadRequest("Value must be passed in the request body.");
+            return Ok(repository[id]);
+        }
+
+        [HttpPost]
+        public Reservation Post([FromBody] Reservation res) =>
+        repository.AddReservation(new Reservation
+        {
+            Name = res.Name,
+            StartLocation = res.StartLocation,
+            EndLocation = res.EndLocation
+        });
+
+        [HttpPut]
+        public Reservation Put([FromBody] Reservation res) => repository.UpdateReservation(res);
+
+        [HttpPatch("{id}")]
+        public StatusCodeResult Patch(int id, [FromBody] JsonPatchDocument<Reservation> patch)
+        {
+            var res = (Reservation)((OkObjectResult)Get(id).Result).Value;
+            if (res != null)
+            {
+                patch.ApplyTo(res);
+                return Ok();
+            }
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id) => repository.DeleteReservation(id);
+    }
+}
